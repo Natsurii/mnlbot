a00fd5daedc7f22885d542de86ae6ccca3c91e2d
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-04-28 14:47:13 +0800

    update the poster, remove some func

--- poster.py
+++ poster.py
@@ -4,18 +4,15 @@ import facebook
 import os
 
 #login
-access_token = os.environ['TOKEN'] #
+access_token = os.environ['TOKEN'] 
 graph = facebook.GraphAPI(access_token=access_token, version="3.1")
-
-# Init
-page = graph.get_object(id='860133054332205', fields='name')
-print('Page target on ' + page['name'] +' with id ' + page['id'])
 
 #upload post
 text_file1 = open("adj.txt", "r")
 adj = random.choice(text_file1.readlines())
 text_file2 = open("mnlmem.txt", "r")
-oshis = random.choice(text_file2.readlines())
+oshi = random.choice(text_file2.readlines())
+oshis = oshi.replace('\n','')
 
 post = graph.put_photo(image=open(f'images/{oshis}.png', 'rb'),
                 message=f'Our oshi MNL48 {oshis} is {adj}')
