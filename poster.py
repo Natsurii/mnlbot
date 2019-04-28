976fc16df49855757475c05620ca95056b1e4f6b
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-04-28 21:53:44 +0800

    Whitespace fix

--- poster.py
+++ poster.py
@@ -15,7 +15,7 @@ oshi = random.choice(text_file2.readlines())
 oshis = oshi.replace('\n','')
 
 post = graph.put_photo(image=open(f'images/{oshis}.png', 'rb'),
-                message=f'Our oshi MNL48 {oshis} is {adj}')
+                message=f'Our oshi MNL48 {oshis} is{adj}.')
 
 graph.put_object(parent_object=post['post_id'], connection_name='comments',
                   message='Is this true?')
