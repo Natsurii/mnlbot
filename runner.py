b93dc514b9f9f047e2d89b52beec62d2b3a0beaf
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-05-03 22:41:07 +0800

    migrated to twitter api > iftt > facebook page

--- runner.py
+++ runner.py
@@ -3,10 +3,10 @@ import schedule
 import time
 import os
 
-os.system('python poster.py') #For script boot run
+os.system('python tweeter.py') #For script boot run
 
 def job(): #for scheduled run
-    os.system('python poster.py')
+    os.system('python tweeter.py')
     print('Process ran succesfully.')
 
 schedule.every().hour.do(job)
