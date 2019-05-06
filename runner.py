19aa8cc44fc83fb1e54b7fee8874113efb15d86d
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-05-06 13:49:36 +0800

    code refactor, cron job at 25 * * * *

--- runner.py
+++ runner.py
@@ -2,15 +2,15 @@
 import schedule
 import time
 import os
-
-os.system('python tweeter.py') #For script boot run
+from datetime import datetime
 
 def job(): #for scheduled run
     os.system('python tweeter.py')
-    print('Process ran succesfully.')
+    print(f'Process ran succesfully. \n Time: {str(datetime.now())}')
 
-schedule.every().hour.do(job)
+schedule.every().hour.do(job).at(:25) # run every xx:25:xx / 25 * * * * on cron 
     
 while 1:
     schedule.run_pending()
     time.sleep(1)
+    #print(str(schedule.Scheduler.next_run), 'scheduled to run') #??? working 
