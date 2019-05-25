0dd2f002f59336e689a46610148d9b045c6766f4
Natsurii <unatsurii@gmail.com>
2019-05-25 15:14:08 +0800

    Signed-off-by: Nice
    Signed-off-by: Andrei James Drice <anounymousandrei@gmail.com>
    Signed-off-by: Natsurii <unatsurii@gmail.com>

--- FB-Tweet.py
+++ FB-Tweet.py
@@ -5,6 +5,7 @@ import random
 import logging
 import schedule
 import time
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 from urllib.request import urlopen
 import urllib.request
 from bs4 import BeautifulSoup
@@ -30,6 +31,7 @@ consumer_secret = keys['CONSECRET']
 twitter_token =  keys['ACCTOKEN']
 acc_secret = keys['TOKENSEC']
 fb_token = keys['TOKEN_DEBUG'] 
+
 '''
 #scrapers
 def profilescrape(oshi:str):
@@ -44,23 +46,54 @@ def profilescrape(oshi:str):
 	imgurls = random.choice(urls)
 	urllib.request.urlretrieve(imgurls, "oshi.png")
 
+def image(oshi, adjs):
+
+	html = urlopen(f'https://mnl48.hallohallo.com/profile/{oshi}')
+	soup = BeautifulSoup(html, 'html.parser')
+	imgs = soup.find_all('img', attrs={'height':'360'})
+	urls =[]
+	for i in imgs:
+		f= i['src']
+		urls.append(f)
+	urls
+	imgurls = random.choice(urls)
+	urllib.request.urlretrieve(imgurls, "oshi.png")
+
+	size = width, height = 600, 338;
+
+	image = Image.new('RGB', size, 'white') ; profile = Image.open('oshi.png') ; bg = Image.open('bg.png')
+	image.paste(bg,(0,0)) ;
+	w, h = profile.size
+	cropped = profile.crop((54, 0, w-54, 360))
+
+	out = cropped.resize((193,275), Image.LANCZOS)
+
+	image.paste(out,(45,32))
+	draw = ImageDraw.Draw(image)
+	GothamBold20 = ImageFont.truetype('GothamBold.ttf', 20) ;GothamBold50 = ImageFont.truetype('GothamBold.ttf', 30)
+	draw.text((310, 230),f'Our MNL48 {oshi} is', font=GothamBold20, fill='#000000')
+	draw.text((305, 250),f'{adjs}.', font=GothamBold50, fill='#000000')	
+	image.save('outfile.png','PNG')
+	del image
+	del draw
+
 #Posters
-def facebookpost(oshi:str, adjective:str, file):
+def facebookpost(oshi:str, adjective:str):
 	'''Posting the object to Facebook via GRAPH APi.'''
 	graph = facebook.GraphAPI(access_token=fb_token, version="3.1")
-	post = graph.put_photo(image=open(file,"rb"),
+	post = graph.put_photo(image=open('outfile.png',"rb"),
                 message=f'Our oshi MNL48 {oshi} is {adjective}.')
 
 	graph.put_object(parent_object=post['post_id'], connection_name='comments',
                   message='Is this true?')
 
-def twitterpost(oshi:str, adjective:str, file):
+def twitterpost(oshi:str, adjective:str):
 	'''Posting the object to Twitter via TWITTER APi.'''
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 	auth.set_access_token(twitter_token, acc_secret) 
 	api = tweepy.API(auth) 
 
-	image_path = file
+	image_path = 'outfile.png'
 	tweet = f'Our oshi MNL48 {oshi} is {adjective}.'
 	status = api.update_with_media(image_path, tweet)
 
@@ -77,19 +110,19 @@ def revthread():
 	oshiran = random.choice(text_file2.readlines())
 	oshis = oshiran.replace('\n','')
 
-	file = profilescrape(oshi=oshis)
+	image(oshi=oshis, adjs=adj)
 
 	def fbthread():
 		'''Running for fb thread.'''
 		logging.debug('Starting Facebook Thread')
-		facebookpost(oshi=oshis, adjective=adj,file='oshi.png')
+		facebookpost(oshi=oshis, adjective=adj)
 		logging.debug('=====================SUCCESS POSTING FB, Exiting....=====================')
 
 
 	def twitterthread():
 		'''Running for twitter thread.'''
 		logging.debug('Starting Twitter Thread')
-		twitterpost(oshi=oshis, adjective=adj, file='oshi.png')
+		twitterpost(oshi=oshis, adjective=adj)
 		logging.debug('=====================SUCCESS POSTING TW,Exiting....=====================')
 	
 	fb = threading.Thread(name='Facebook API', target=fbthread)	
@@ -97,9 +130,6 @@ def revthread():
 
 	fb.start()
 	tw.start()
-
-	tw.exit()
-	fb.exit()
 	
 schedule.every().hour.at(':35').do(revthread) # run every xx:35:xx / 35 * * * * on cron 
 schedule.every().hour.at(':05').do(revthread) # run every xx:05:xx / 5 * * * * on cron 
